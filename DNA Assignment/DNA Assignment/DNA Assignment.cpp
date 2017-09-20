1a40cb693b994d7f48f394beb33e3b690e6deacd
Trevor <jonesmrtrevor@gmail.com>
2017-09-19 23:51:44 -0600

    DNA stuff

---
+++ DNA Assignment/DNA Assignment/DNA Assignment.cpp
@@ -0,0 +1,78 @@
+// DNA Assignment.cpp : Defines the entry point for the application.
+//
+
+#include <iostream>
+using namespace std;
+
+/**********************************************************************
+* Add text here to describe what the function "main" does. Also don't forget
+* to fill this out with meaningful text or YOU WILL LOSE POINTS.
+***********************************************************************/
+int getUserDNA()
+{
+	string myDNA;
+	cout << "Enter your DNA sequence: ";
+	cin >> myDNA;
+
+	return 0;
+}
+
+/**********************************************************************
+* Add text here to describe what the function "main" does. Also don't forget
+* to fill this out with meaningful text or YOU WILL LOSE POINTS.
+***********************************************************************/
+
+int relativesDNA()
+{
+	string relNames[50];
+	string relDNA[50];
+	int pots;
+	cout << "Enter the number of potential relatives: ";
+	cin >> pots;
+	cout << endl;
+
+	for (int i = 0; i < pots; i++)
+	{
+		cout << "Please enter the name of relative #" << i + 1 << ": ";
+		cin >> relNames[i];
+
+	}
+
+	cout << endl;
+
+	for (int a = 0; a < pots; a++)
+	{
+		cout << "Please enter the DNA sequence for " << relNames[a] << ": ";
+		cin >> relDNA[a];
+
+	}
+	cout << endl;
+
+	return 0;
+}
+
+void percent(string myDNA, string relNames[], string relDNA[], int pots)
+{
+	for (int i = 0; i < pots; i++)
+	{
+		int b = 0;
+		for (int a = 0; a < 10; a++)
+		{
+			if (myDNA[a] == relDNA[i][a])
+				b++;
+		}
+		cout << "Percent Match for " << relNames[i] << ": ";
+		cout << b * 10 << "%" << endl;
+	}
+
+	return;
+}
+
+int main()
+{
+	getUserDNA();
+	relativesDNA();
+	percent(myDNA, relNames, relDNA, pots);
+
+	return 0;
+}
