26e478af74c2e7af2d98d4d85f940820090b5f3d
Mike Jones <jonesmr_pub1@comcast.net>
2017-09-20 00:31:29 -0600

    Added vector and class for relatives

--- DNA Assignment/DNA Assignment/DNA Assignment.cpp
+++ DNA Assignment/DNA Assignment/DNA Assignment.cpp
@@ -3,9 +3,15 @@
 
 #include <iostream>
 #include <sstream>
+#include <vector>
 
 using namespace std;
 
+class RelativesDNA {
+public:
+	string m_name;
+	string m_dna;
+};
 /**********************************************************************
 * Add text here to describe what the function "main" does. Also don't forget
 * to fill this out with meaningful text or YOU WILL LOSE POINTS.
@@ -31,34 +37,36 @@ int getUserDNA(string& myDNA)
 * to fill this out with meaningful text or YOU WILL LOSE POINTS.
 ***********************************************************************/
 
-int relativesDNA()
+int relativesDNA(vector<RelativesDNA>& relatives)
 {
-	string relNames[50];
-	string relDNA[50];
+//	string relNames[50];
+//	string relDNA[50];
 	int pots;
 	cout << "Enter the number of potential relatives: ";
 	cin >> pots;
 	cout << endl;
+	RelativesDNA relative;
 
 	for (int i = 0; i < pots; i++)
 	{
 		cout << "Please enter the name of relative #" << i + 1 << ": ";
-		cin >> relNames[i];
-
+		cin >> relative.m_name;
+		relatives.push_back(relative);
 	}
 
 	cout << endl;
 
 	for (int a = 0; a < pots; a++)
 	{
-		cout << "Please enter the DNA sequence for " << relNames[a] << ": ";
-		cin >> relDNA[a];
+		cout << "Please enter the DNA sequence for " << relatives[a].m_name << ": ";
+		cin >> relatives[a].m_dna;
 
 	}
 	cout << endl;
 
 	return 0;
 }
+
 
 void percent(string myDNA, string relNames[], string relDNA[], int pots)
 {
@@ -77,14 +85,17 @@ void percent(string myDNA, string relNames[], string relDNA[], int pots)
 	return;
 }
 
+
 int main()
 {
 	string userDNA;
+	vector<RelativesDNA> relatives;
 	userDNA = getUserDNA();
 	// or
 	getUserDNA(userDNA);
-	relativesDNA();
+	relativesDNA(relatives);
+#if 0
 	percent(userDNA, relNames, relDNA, pots);
-
+#endif
 	return 0;
 }
